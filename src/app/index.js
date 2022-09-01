2606523b6281d67ab53f69be558e232c388f2edd
smallLabel <1007243939@qq.com>
2022-09-02 00:41:00 +0800

    完成数据库接入

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@
  *  Author: lijunhong
  *  Date: 2022-09-01 00:44:29
  *  Email: lijunhong@fengmap.com
- *  LastEditTime: 2022-09-01 23:09:56
+ *  LastEditTime: 2022-09-01 23:31:11
  *  LastEditors: lijunhong
  *  LastEditorsEmail: lijunhong@fengmap.com
  *  Description:app文件
@@ -13,6 +13,7 @@ const KoaBody = require("koa-body");
 const userRouter = require("../router/user.route");
 
 const app = new Koa();
+
 app.use(KoaBody());
 
 app.use(userRouter.routes());
---
+++ src/db/seq.js
@@ -0,0 +1,35 @@
+/*
+ *  Author: lijunhong
+ *  Date: 2022-09-02 00:04:33
+ *  Email: lijunhong@fengmap.com
+ *  LastEditTime: 2022-09-02 00:39:42
+ *  LastEditors: lijunhong
+ *  LastEditorsEmail: lijunhong@fengmap.com
+ *  Description:
+ *  Copyright: Copyright 2014 - 2022, FengMap, Ltd. All rights reserved.
+ */
+
+const { Sequelize } = require("sequelize");
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PWD,
+  MYSQL_DB,
+} = require("../config/config.default");
+
+const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
+  host: MYSQL_HOST,
+  dialect: "mysql",
+});
+
+seq
+  .authenticate()
+  .then(() => {
+    console.log("链接成功");
+  })
+  .catch((err) => {
+    console.log("链接失败", err);
+  });
+
+module.exports = seq;
